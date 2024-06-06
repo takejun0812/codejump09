72c0ad4ba0ae015e653a2015de465205741df6c6
takejun0812 <junta.takenochi@prum.jp>
2024-06-06 13:04:09 +0900

    add/mainsection

--- script.js
+++ script.js
@@ -4,4 +4,20 @@ $(function () {
         $('.burger_btn').toggleClass('close');
         $('.nav-wrapper').toggleClass('fade');
     })
+
+    $('.slide-items').slick({
+        centerMode: true,
+        slidesToShow: 5,
+        centerPadding: '30%',
+        variableWidth: true,
+
+        responsive: [{
+            breakpoint: 600,
+             settings: {
+              slidesToShow: 3,
+              centerPadding: '10%',
+             },
+            },
+          ]
+	});
 });
